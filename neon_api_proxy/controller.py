e871a2aa8ea9aee0eea045bcf87bb4e9cd62c46e
kirill <kirill.grim@gmail.com>
2021-07-12 16:33:10 +0300

    Initial implementation of generic service controller

---
+++ neon_api_proxy/controller.py
@@ -0,0 +1,51 @@
+import os
+import json
+
+from neon_api_proxy.owm_api import OpenWeatherAPI
+from neon_api_proxy.alpha_vantage_api import AlphaVantageAPI
+from neon_api_proxy.wolfram_api import WolframAPI
+
+
+class APIController:
+    """
+        Generic module for binding between service name and actual service for fulfilling request
+    """
+
+    # Mapping between string service name and actual class
+    service_class_mapping = {
+        'wolfram_alpha': WolframAPI,
+        'alpha_vantage': AlphaVantageAPI,
+        'open_weather_map': OpenWeatherAPI
+    }
+
+    def __init__(self, config: str = None):
+        """
+            @param config: Path to the local configuration file
+        """
+        self.config = None
+
+        if os.environ.get('ENV', None) == 'DEV' and config:
+            config_path = os.path.expanduser(config)
+            if os.path.exists(config_path):
+                with open(os.path.expanduser(config_path)) as config_file:
+                    self.config = json.load(config_file)
+
+    def resolve_query(self, query: dict) -> dict:
+        """
+            Generically resolves input query dictionary by mapping its "service" parameter
+            @param query: dictionary with query parameters
+            @return: response from the destination service
+        """
+        target_service = query.get('service', None)
+        if target_service and target_service in list(self.service_class_mapping):
+            api_key = None
+            if self.config:
+                api_key = self.config['SERVICES'][target_service]['api_key']
+            resp = self.service_class_mapping[target_service](api_key=api_key).handle_query(**query)
+        else:
+            resp = {
+                "status_code": 401,
+                "content": f"Unresolved service name: {target_service}",
+                "encoding": "utf-8"
+            }
+        return resp
---
+++ tests/test_generic_controller.py
@@ -0,0 +1,87 @@
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+#
+# Copyright 2008-2021 Neongecko.com Inc. | All Rights Reserved
+#
+# Notice of License - Duplicating this Notice of License near the start of any file containing
+# a derivative of this software is a condition of license for this software.
+# Friendly Licensing:
+# No charge, open source royalty free use of the Neon AI software source and object is offered for
+# educational users, noncommercial enthusiasts, Public Benefit Corporations (and LLCs) and
+# Social Purpose Corporations (and LLCs). Developers can contact developers@neon.ai
+# For commercial licensing, distribution of derivative works or redistribution please contact licenses@neon.ai
+# Distributed on an "AS IS” basis without warranties or conditions of any kind, either express or implied.
+# Trademarks of Neongecko: Neon AI(TM), Neon Assist (TM), Neon Communicator(TM), Klat(TM)
+# Authors: Guy Daniels, Daniel McKnight, Regina Bloomstine, Elon Gasper, Richard Leeds
+#
+# Specialized conversational reconveyance options from Conversation Processing Intelligence Corp.
+# US Patents 2008-2021: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
+# China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
+
+import os
+import sys
+import unittest
+from time import sleep
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from neon_api_proxy.controller import APIController
+
+INVALID_SERVICE_QUERY = {
+    "service": "invalid_service",
+    "query": "how far away is Rome?",
+    "api": "simple",
+    "units": "metric",
+    "ip": "64.34.186.120"
+}
+
+VALID_ALPHA_VANTAGE_QUERY = {
+    "service": "alpha_vantage",
+    "api": "quote",
+    "symbol": "GOOGL"
+}
+
+VALID_OWM_QUERY = {
+    "service": "open_weather_map",
+    "lat": "47.6769",
+    "lng": "-122.2060",
+    "units": "imperial"
+}
+
+VALID_WOLFRAM_QUERY = {
+    "service": "wolfram_alpha",
+    "query": "how far away is Rome?",
+    "api": "simple",
+    "units": "metric",
+    "ip": "64.34.186.120"
+}
+
+
+class TestCachedAPI(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.controller = APIController(config='config.json')
+
+    def test_invalid_service(self):
+        resp = self.controller.resolve_query(INVALID_SERVICE_QUERY)
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp['status_code'], 401)
+
+    @unittest.skip('No API key provided')
+    def test_alpha_vantage_forwarding(self):
+        resp = self.controller.resolve_query(VALID_ALPHA_VANTAGE_QUERY)
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp['status_code'], 200)
+
+    @unittest.skip('No API key provided')
+    def test_owm_forwarding(self):
+        resp = self.controller.resolve_query(VALID_OWM_QUERY)
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp['status_code'], 200)
+
+    def test_wolfram_forwarding(self):
+        resp = self.controller.resolve_query(VALID_WOLFRAM_QUERY)
+        self.assertIsNotNone(resp)
+        self.assertEqual(resp['status_code'], 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
