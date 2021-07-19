d5d0b5c38f81597a85cde36e24e805cdb4bd4f68
kirill <kirill.grim@gmail.com>
2021-07-19 20:16:05 +0300

    Renamed to the new signature

--- neon_api_proxy/socket_handler.py
+++ neon_api_proxy/socket_handler.py
@@ -11,10 +11,10 @@ class NeonAPITCPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
         received_message = get_packet_data(self.request)
-        received_message_decoded = decode_b64_msg(received_message)
+        received_message_decoded = b64_to_dict(received_message)
         LOG.debug(f"Received request from '{self.client_address[0]}' : {received_message_decoded}")
         response = self.server.controller.resolve_query(received_message_decoded)
         LOG.debug(f'Received response from controller: {response}')
-        encoded_response = encode_b64_msg(response)
+        encoded_response = dict_to_b64(response)
         LOG.debug(f'Encoded response from controller: {encoded_response}')
         self.request.sendall(encoded_response)
