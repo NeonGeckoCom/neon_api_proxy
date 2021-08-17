369592fba364403f132f58c8bb703848e0124568
Daniel McKnight <daniel@neongecko.com>
2021-08-17 12:17:20 -0700

    Fix typo in `get_mq_config` Update consumer registration to use convenience method Handle re-registration and restart of consumers on error

--- neon_api_proxy/config.py
+++ neon_api_proxy/config.py
@@ -48,7 +48,7 @@ def get_mq_config() -> dict:
     Locates a valid MQ config for MQ Authentication
     :return: dict containing "MQ" key with server and users configurations
     """
-    if environ.get('NEON_MQ_CONFIG_PATH', 'config.json'):
+    if path.isfile(environ.get('NEON_MQ_CONFIG_PATH', 'config.json')):
         valid_config_path = environ.get('NEON_API_PROXY_CONFIG_PATH', 'config.json')
     elif path.isfile(path.expanduser("~/.config/neon/mq_config.json")):
         valid_config_path = path.expanduser("~/.config/neon/mq_config.json")
